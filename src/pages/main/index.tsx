b88e3686fe550d8dbb23545c759854bf88f0b7bb
iksoon <jjsk109@gmail.com>
2022-11-27 23:19:49 +0900

    구분 전 파일 명칭을 위한 커밋

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -21,9 +21,93 @@ const Main = () => {
 
   return (
     <div>
-      <button onClick={changeName}>사람 이름 변경</button>
-      <h1>이름 :</h1>
-      <span>{people.name}</span>
+      <header>
+        <div>여행앤유</div>
+        <div>검색</div>
+      </header>
+      <div>
+        <div>
+          <button>필터버튼</button>
+        </div>
+        <div>
+          <div>자연</div>
+          <div>관광</div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <div>취향에 따른 추천 여행</div>
+            <div>더보기</div>
+          </div>
+        </div>
+        <div>
+          <ul>
+            {/* 카드 */}
+            <li>
+              <img src='' alt='' />
+              <div>하트</div>
+              <div>
+                <div>누가누가</div>
+                <div>~~~~~~`</div>
+              </div>
+            </li>
+            <li>
+              <img src='' alt='' />
+              <div>하트</div>
+              <div>
+                <div>누가누가</div>
+                <div>~~~~~~`</div>
+              </div>
+            </li>
+            <li>
+              <img src='' alt='' />
+              <div>하트</div>
+              <div>
+                <div>누가누가</div>
+                <div>~~~~~~`</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <div>추천여행지</div>
+            <div>더보기</div>
+          </div>
+        </div>
+        <div>
+          <ul>
+            {/* 카드 */}
+            <li>
+              <img src='' alt='' />
+              <div>하트</div>
+              <div>
+                <div>누가누가</div>
+                <div>~~~~~~`</div>
+              </div>
+            </li>
+            <li>
+              <img src='' alt='' />
+              <div>하트</div>
+              <div>
+                <div>누가누가</div>
+                <div>~~~~~~`</div>
+              </div>
+            </li>
+            <li>
+              <img src='' alt='' />
+              <div>하트</div>
+              <div>
+                <div>누가누가</div>
+                <div>~~~~~~`</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
