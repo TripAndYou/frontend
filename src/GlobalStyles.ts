a0572a454caaec5e4e2deefd7d4a7a6dc6ef1fd8
은배 <geb9211@gmail.com>
2022-12-08 11:45:12 +0900

    [Design] width 360px로 설정

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -5,6 +5,8 @@ const GlobalStyles = createGlobalStyle`
   ${reset}
   body {
     font-family: "Pretendard", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+    width: 360px;
+    margin: 0 auto;
   }
 `
 
