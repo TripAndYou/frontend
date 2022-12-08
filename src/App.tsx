d92987be41e9bf9ab6b7f4a53403489b6e23ead3
은배 <geb9211@gmail.com>
2022-12-08 11:46:19 +0900

    [Rename] GlobalStyles 컴포넌트를 index.tsx로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import GlobalStyles from './GlobalStyles'
-import { MainStyle } from './AppStyles'
 import Routers from './router'
 
 function App() {
   return (
-    <MainStyle>
-      <GlobalStyles />
+    <>
       <Routers />
-    </MainStyle>
+    </>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
+import GlobalStyles from './GlobalStyles'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyles />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
