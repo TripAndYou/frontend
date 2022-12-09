f8c35339d6d238f27834e009bae3b7e418dfa55b
은배 <geb9211@gmail.com>
2022-12-09 15:41:08 +0900

    [Refactor] search 파일 컴포넌트 분리

--- src/pages/Search/Search.styles.ts
+++ src/pages/Search/Search.styles.ts
@@ -1,62 +1,5 @@
 import Styled from 'styled-components'
 
-export const SearchContainer = Styled.section`
-  width: 100%;
-  height: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-
-export const BackButton = Styled.button`
-  all: unset;
-  cursor: pointer;
-`
-
-export const SearchInputContainer = Styled.div`
-  position: relative;
-  width: 300px;
-  height: 48px;
-  margin-left: 10px;
-`
-
-export const SearchInput = Styled.input`
-  all: unset;
-  box-sizing: border-box;
-  position: relative;
-  width: 100%;
-  height: 100%;
-  background: var(--color-neutral-200);
-  padding: 0 16px;
-
-  &:focus {
-    z-index: 5;
-  }
-`
-
-export const SearchInputLabel = Styled.label`
-  position: absolute;
-  top: 12px;
-  left: 16px;
-  color: var(--color-neutral-500);
-  font-weight: bold;
-  cursor: text;
-`
-
-export const SearchInputLabelText = Styled.span`
-  display: inline-block;
-  position: absolute;
-  width: 240px;
-  height: 24px;
-  top: 0px;
-  left: 28px;
-  color: var(--color-neutral-500);
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 24px;
-`
-
 // contents
 export const SearchContentsContainer = Styled.section`
   width: 328px;
@@ -80,26 +23,6 @@ export const SearchContentsTitle = Styled.h2`
 export const RecentSearchResultContainer = Styled.div`
   display: flex;
   flex-direction: column;
-`
-
-export const RecentSearchResult = Styled.div`
-  margin-top: 16px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`
-
-export const RecentSearchResultText = Styled.span`
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 20px;
-  color: var(--color-neutral-800);
-`
-
-export const RecentSearchResultDeleteBtn = Styled.button`
-  all: unset;
-  cursor: pointer;
-  padding: 0 10px;
 `
 
 // popular spot
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,68 +1,11 @@
 import * as S from './Search.styles'
 import { popularSpotList } from '../../utils'
-
-const RecentSearchResult = ({ text }: { text: string }) => {
-  return (
-    <S.RecentSearchResult>
-      <S.RecentSearchResultText>{text}</S.RecentSearchResultText>
-      <S.RecentSearchResultDeleteBtn>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='14'
-          height='14'
-          viewBox='0 0 14 14'
-          fill='none'
-        >
-          <path
-            d='M2 2L7 7M7 7L12 12M7 7L12 2M7 7L2 12'
-            stroke='#999999'
-            strokeWidth='2'
-            strokeLinecap='square'
-            strokeLinejoin='round'
-          />
-        </svg>
-      </S.RecentSearchResultDeleteBtn>
-    </S.RecentSearchResult>
-  )
-}
+import { SearchHeader, RecentSearchResult } from '../../components/Search'
 
 const Search = () => {
   return (
     <>
-      <S.SearchContainer>
-        <S.BackButton>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            fill='none'
-          >
-            <path d='M16 22L6 12L16 2L17.775 3.775L9.55 12L17.775 20.225L16 22Z' fill='#222222' />
-          </svg>
-        </S.BackButton>
-        <S.SearchInputContainer>
-          <S.SearchInput id='searchInput' />
-          <S.SearchInputLabel htmlFor='searchInput'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='21'
-              height='21'
-              viewBox='0 0 21 21'
-              fill='none'
-            >
-              <path
-                d='M19 19L13.8033 13.8033M13.8033 13.8033C15.1605 12.4461 16 10.5711 16 8.5C16 4.35786 12.6421 1 8.5 1C4.35786 1 1 4.35786 1 8.5C1 12.6421 4.35786 16 8.5 16C10.5711 16 12.4461 15.1605 13.8033 13.8033Z'
-                stroke='#999999'
-                strokeWidth='2'
-                strokeLinecap='square'
-                strokeLinejoin='round'
-              />
-            </svg>
-            <S.SearchInputLabelText>여행지를 검색해보세요</S.SearchInputLabelText>
-          </S.SearchInputLabel>
-        </S.SearchInputContainer>
-      </S.SearchContainer>
+      <SearchHeader />
       <S.SearchContentsContainer>
         <S.RecentSearchContainer>
           <S.SearchContentsTitle>최근 검색어</S.SearchContentsTitle>
