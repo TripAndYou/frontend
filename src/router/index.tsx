98ae85d6cf08be2afaacc03a80f769d16d68dc5e
은배 <geb9211@gmail.com>
2022-12-11 00:27:51 +0900

    [rename] login 컴포넌트 경로 삭제

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
-import { Login, Search } from '../pages'
+import { Search } from '../pages'
 import Main from '../pages/main'
 
 const Routers = () => {
   return (
     <Routes>
       <Route path='/' element={<Main />} />
-      <Route path='/login' element={<Login />} />
       <Route path='/search' element={<Search />} />
     </Routes>
   )
